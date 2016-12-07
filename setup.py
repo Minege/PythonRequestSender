20c422841481bcf2d2ef9bed67534a2c43535620
Minege <minege02@gmail.com>
2016-12-07 17:54:28 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="PythonRequestSender",
+        version="1.0",
+        description="A Request sender, with multiple web agent, etc",
+        author="Minege",
+        py_module=['pythonrequestsender']
+)
